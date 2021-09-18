551ac6715d7065d6e9079b0f2bbb5ae9d900b511
BengiOnMAC <356844587@qq.com>
2021-09-19 00:36:43 +0800

    Mabatis layer config for SSM framework

---
+++ src/main/java/com/kuang/service/BookServiceImpl.java
@@ -0,0 +1,35 @@
+package com.kuang.service;
+
+import com.kuang.dao.BookMapper;
+import com.kuang.pojo.Books;
+
+import java.util.List;
+
+public class BookServiceImpl implements BookService {
+
+    // service调DAO层，所以讲DAO引入进来
+    private BookMapper bookMapper;
+    public void setBookMapper(BookMapper bookMapper) {
+        this.bookMapper = bookMapper;
+    }
+
+    public int addBook(Books books) {
+        return bookMapper.addBook(books);
+    }
+
+    public int deleteBookById(int id) {
+        return bookMapper.deleteBookById(id);
+    }
+
+    public int updateBook(Books books) {
+        return bookMapper.updateBook(books);
+    }
+
+    public Books queryBookById(int id) {
+        return bookMapper.queryBookById(id);
+    }
+
+    public List<Books> queryAllBook() {
+        return bookMapper.queryAllBook();
+    }
+}
